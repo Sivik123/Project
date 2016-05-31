83c5efad3f5e72e42b14b77a49b685cda269b8e1
Sivik123 <robroj8045@interia.pl>
2016-05-31 16:24:00 +0200

    Prostokąt

--- Projekt2/prostokat.cs
+++ Projekt2/prostokat.cs
@@ -16,5 +16,36 @@ namespace Projekt2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a,b;
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            textBox7.Text = (a * b).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = double.Parse(textBox3.Text);
+            b = double.Parse(textBox4.Text);
+            textBox7.Text = (2 * a + 2 * b).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+              double a, b;
+            a = double.Parse(textBox5.Text);
+            b = double.Parse(textBox6.Text);
+            textBox7.Text = ((Math.Sqrt(a * a + b * b)).ToString());
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            figury f = new figury();
+            f.Show();
+        }
     }
 }
