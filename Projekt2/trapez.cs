415e047c85391d0ec906362a4febd36ac4fd84c4
Sivik123 <robroj8045@interia.pl>
2016-05-31 16:57:24 +0200

    Trapez

--- Projekt2/trapez.cs
+++ Projekt2/trapez.cs
@@ -16,5 +16,32 @@ namespace Projekt2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a,b,h;
+            a = double.Parse(textBox1.Text);
+            b = double.Parse(textBox2.Text);
+            h = double.Parse(textBox3.Text);
+            textBox4.Text = (((a +b)*h)/2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a, b, c, d;
+            a = double.Parse(textBox5.Text);
+            b = double.Parse(textBox6.Text);
+            c = double.Parse(textBox7.Text);
+            d = double.Parse(textBox8.Text);
+            textBox4.Text = (a + b + c + d).ToString();
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            figury f = new figury();
+            f.Show();
+        }
     }
 }
